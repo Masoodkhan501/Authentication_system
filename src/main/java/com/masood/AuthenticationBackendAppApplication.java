293bf9d07b04897e970359a13312ce56a3a7586f
Masoodkhan501 <141975307+Masoodkhan501@users.noreply.github.com>
2026-09-05 22:21:00 +0530

    First commit

---
+++ src/main/java/com/masood/AuthenticationBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.masood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationBackendAppApplication.class, args);
+	}
+
+}
